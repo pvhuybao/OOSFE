2be23ba57a938e19bebe0244a694bfc8d900762e
Ta Xuan Tung <tung.tx1993@gmail.com>
2018-03-16 10:58:53 +0700

    update code

--- OOS/src/app/admin/orders/orders.component.ts
+++ OOS/src/app/admin/orders/orders.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OrdersService } from '../services/orders.service';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
-
-  constructor() { }
+  orders:any;
+  constructor(private orderService:OrdersService) { }
 
   ngOnInit() {
+    this.orderService.getOrder().subscribe(data => {this.orders = data});
+  }
+
+  delete(id)
+  {
+    this.orderService.deleteOrder(id).subscribe(data => {
+      this.orderService.getOrder().subscribe(data => {this.orders = data});
+    });
   }
 
 }
--- OOS/src/app/admin/services/orders.service.ts
+++ OOS/src/app/admin/services/orders.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AuthHttpService } from '../../auth/auth-http.service'
 
 @Injectable()
 export class OrdersService {
 
-  constructor() { }
+  constructor(private authHttpService: AuthHttpService) { }
+
+  getOrder() {
+    return this.authHttpService.get("").map(result => result.json() || []);
+  }
+  deleteOrder(id){
+    return this. authHttpService.delete(""+id);
+  }
 
 }
