b535b21dac8aa50fa9441dcfd1ae0cd586a35c7f
DESKTOP-O8TUU0R\TU <daydreamer1804@gmail.com>
2018-03-19 08:59:25 +0700

    update code -Update UI edit order

--- OOS/src/app/admin/orders/edit-order/edit-order.component.ts
+++ OOS/src/app/admin/orders/edit-order/edit-order.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderComponent } from '../../../shopping/order/order.component';
+import { OrdersService } from '../../services/orders.service';
+import { AddressModel } from '../../models/Address';
+import { OrderDetailModel } from '../../models/OrderDetail';
+import { OrdersModel } from '../../models/order';
 
 @Component({
   selector: 'app-edit-order',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ss: OrdersService) { }
 
   ngOnInit() {
+  }
+  
+  edit(){
+    let address : AddressModel = {
+      name : "lon",
+      phone : "113",
+      street : "Lac Long Quan",
+      province : "TP HCM",
+      district : "Tan Binh",
+      type :0
+    }
+
+    let detail : OrderDetailModel = {
+      idProduct : "HH001",
+      nameProduct : 'Xe Dream',
+      quantity : 2,
+      price : 1500,
+      totalPrice : 3000
+    }
+
+    let order : OrdersModel = {
+      email : "da@gmail.com",
+      userId : null,
+      orderDetails : [detail],
+      address : [address],
+      total : 0
+    }
+      this.ss.add(order).subscribe (data => console.log(data));
+      
+    
+
   }
 
 }
