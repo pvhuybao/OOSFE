ecd9d0444035c2f3fcc5556356423a40f35af87b
DESKTOP-O8TUU0R\TU <daydreamer1804@gmail.com>
2018-03-19 11:08:17 +0700

    update code - Edit Order

--- OOS/src/app/admin/admin.module.ts
+++ OOS/src/app/admin/admin.module.ts
@@ -12,21 +12,17 @@ import { UsersComponent } from './users/users.component';
 import { OrderDetailComponent } from './orders/order-detail/order-detail.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { CategoryService } from './services/category.service';
-<<<<<<< HEAD
 import { EditOrderComponent } from './orders/edit-order/edit-order.component'
 import { OverviewCategoriesComponent } from './categories/overview-categories/overview-categories.component'
-=======
 import { UserCreateComponent } from './users/user-create/user-create.component';
 import { UserEditComponent } from './users/user-edit/user-edit.component'
 import { UserService } from './services/user.service';
 import { CreateCategoryComponent } from './categories/createcategory/createcategory.component';
 import { EditCategoryComponent } from './categories/editcategory/editcategory.component'
-
->>>>>>> develop
 import { ProductService } from './services/Product.service';
 import { OrdersService } from './services/orders.service';
 import { CreateOrderComponent } from './orders/create-order/create-order.component';
-import { OverviewCategoriesComponent } from './categories/overview-categories/overview-categories.component';
+
 
 
 
@@ -46,22 +42,16 @@ import { OverviewCategoriesComponent } from './categories/overview-categories/ov
     OrdersComponent, 
     UsersComponent, 
     OrderDetailComponent, 
-<<<<<<< HEAD
     ProductDetailComponent,
     EditOrderComponent,
     OverviewCategoriesComponent,
-    CreateOrderComponent
-  ],
-
-=======
-    ProductDetailComponent, 
-    OverviewCategoriesComponent,
+    CreateOrderComponent,
     UserCreateComponent, 
     UserEditComponent,
     EditCategoryComponent,
     CreateOrderComponent
   ],
->>>>>>> develop
+
   providers: [
     ProductService,
     CategoryService, 
