c6003115358e7212fc16d4bc38f24494f313784e
Ta Xuan Tung <tung.tx1993@gmail.com>
2018-03-22 11:29:05 +0700

    update code

--- OOS/src/app/admin/admin-management/admin-management.component.ts
+++ OOS/src/app/admin/admin-management/admin-management.component.ts
@@ -14,7 +14,13 @@ export class AdminManagementComponent implements OnInit {
 
   ngOnInit() {
     this.breadcrumbService.addFriendlyNameForRoute('/admin', 'Admin');
-    this.breadcrumbService.addFriendlyNameForRoute('/admin/manager', 'Manager'); 
+    this.breadcrumbService.addFriendlyNameForRoute('/admin/manager', 'Manager');
+    this.breadcrumbService.addFriendlyNameForRoute('/admin/manager/orders', 'Orders');
+    this.breadcrumbService.addFriendlyNameForRoute('/admin/manager/orders/create', 'Create'); 
+    this.breadcrumbService.addFriendlyNameForRoute('/admin/manager/products', 'Products'); 
+    this.breadcrumbService.addFriendlyNameForRoute('/admin/manager/users', 'Users');
+    this.breadcrumbService.addFriendlyNameForRoute('/admin/manager/users/create', 'Create');
+    this.breadcrumbService.addFriendlyNameForRoute('/admin/manager/categories/create', 'Create');  
   }
 
 }
