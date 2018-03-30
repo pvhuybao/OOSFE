7e1bb062a9ff7a1a08328ea3f3ba119c750f8f51
trantienphat <trantienphat1110@gmail.com>
2018-03-30 10:19:16 +0700

    Nothing! It's Commit to pull

--- OOS/src/app/admin/orders/orders.component.ts
+++ OOS/src/app/admin/orders/orders.component.ts
@@ -17,72 +17,92 @@ export class OrdersComponent implements OnInit {
   listStatus = new Array<StatusOrder>()
   orderToDelete: OrdersModel;
 
-  email: string="";
-  phone: string="";
-  page: number=1;
+  email: string = "";
+  phone: string = "";
+  page: number = 1;
+  pageCount: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
 
-  status: string;
+  pFirst: number;
+  pCenter: number;
+  pLast: number;
+  active = "active";
+
   constructor(
-    private ordersService: OrdersService, 
-    private router: Router, 
+    private ordersService: OrdersService,
+    private router: Router,
 
-    private spinnerService: SpinnerService) 
-    { 
-      this.listStatus.push(new StatusOrder(0,"Confirming"))
-      this.listStatus.push(new StatusOrder(1,"Confirmed"))
-      this.listStatus.push(new StatusOrder(2,"Shipping"))
-      this.listStatus.push(new StatusOrder(3,"Shipped"))
-      
-    }
+    private spinnerService: SpinnerService) {
+    this.listStatus.push(new StatusOrder(0, "Confirming"))
+    this.listStatus.push(new StatusOrder(1, "Confirmed"))
+    this.listStatus.push(new StatusOrder(2, "Shipping"))
+    this.listStatus.push(new StatusOrder(3, "Shipped"))
 
-   
+  }
+
+
 
   ngOnInit() {
     this.getOrderList();
   }
 
-  getOrderList() 
-  {
+  getOrderList() {
     this.spinnerService.startLoadingSpinner();
-    this.ordersService.getList(this.email,this.phone,this.page).subscribe(data=>{
+    this.ordersService.getList(this.email, this.phone, this.page).subscribe(data => {
       this.spinnerService.turnOffSpinner();
       this.listOrders = data.items;
+      this.pageCount = data.pageCount;
     });
   }
 
-  get(orderId)
-  {
-    this.ordersService.getById(orderId).subscribe(data => {this.orderToDelete = data});
+  choosePage(page: string) {
+
+    // for (let i = 1; i <= page.totalPages; i++) {
+    //   if (i === first || i === last || activePage === i
+    //     || last < 5
+    //     || activePage - 1 === i || (activePage < 3 && i < 4)
+    //     || activePage + 1 === i || (activePage > last - 2 && i > last - 3)
+    //   ) {
+    //     pageNumbers = [...pageNumbers, i];
+    //   }
+    // }
+    // if(page!=="next" && page!=="Previous")
+    // {
+    //     this.page = parseInt(page);
+    //     console.log(this.page);
+    // }
   }
 
-  delete()
-  {
+  get(orderId) {
+    this.ordersService.getById(orderId).subscribe(data => { this.orderToDelete = data });
+  }
+
+  delete() {
     this.spinnerService.startLoadingSpinner();
-    this.ordersService.delete(this.orderToDelete.id).subscribe(()=> {
+    this.ordersService.delete(this.orderToDelete.id).subscribe(() => {
       this.spinnerService.turnOffSpinner();
       this.getOrderList();
       console.log(this.orderToDelete.id);
     });
   }
 
-  edit (order)
-  {
+  edit(order) {
     this.ordersService.sendData(order);
     this.router.navigateByUrl("/admin/manager/orders/edit/" + order.id);
 
   }
 
-  updateStatus(order: OrdersModel)
-  {
+  updateStatus(order: OrdersModel) {
     this.spinnerService.startLoadingSpinner();
     // order.status = 
-    this.ordersService.getById(order.id).subscribe(ord=>{
+    this.ordersService.getById(order.id).subscribe(ord => {
       ord.status = order.status;
-      this.ordersService.put(ord.id,ord).subscribe(data => {
+      this.ordersService.put(ord.id, ord).subscribe(data => {
         this.spinnerService.turnOffSpinner();
         console.log(order.status);
       });
     });
-    
+
   }
 }
