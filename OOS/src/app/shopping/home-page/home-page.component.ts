2c7a6d657867d9284f685e0b5714a95e3cc94a31
loinguyenhuu <35024018+loinguyenhuu@users.noreply.github.com>
2018-04-02 13:31:03 +0700

    update code top discount

--- OOS/src/app/shopping/home-page/home-page.component.ts
+++ OOS/src/app/shopping/home-page/home-page.component.ts
@@ -54,6 +54,7 @@ export class HomePageComponent implements OnInit {
     //   price: 3344, description: "", image: "", idCategory: ""
     // },]
     this.getListNewestProduct();
+    this.getListTopDiscountProduct();
   }
 
   getListNewestProduct() {
@@ -65,13 +66,13 @@ export class HomePageComponent implements OnInit {
 
   getListTopSalesProduct() {
     this.productService.getProductsByParameter("topSales").subscribe(newestProduct => {
-      this.newestProduct = newestProduct
+      this.newestProduct = newestProduct;
     });
   }
 
   getListTopDiscountProduct() {
-    this.productService.getProductsByParameter("topDiscount").subscribe(newestProduct => {
-      this.newestProduct = newestProduct
+    this.productService.getProductsByParameter("topDiscount").subscribe(topDiscount => {
+      this.topDiscount = topDiscount;
     });
   }
 
